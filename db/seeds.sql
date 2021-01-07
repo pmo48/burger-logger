fee95fef6b5654f3ae2eba3bf0c285adef878e2e
pmo48 <pmoriarty100@gmail.com>
2021-01-06 18:52:02 -0800

    db seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and Bleu Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom and Swiss Burger', FALSE);
