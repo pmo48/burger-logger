640e5db7311c9234ebe2fd0fffc41d516f827f48
pmo48 <pmoriarty100@gmail.com>
2021-01-08 15:21:26 -0800

    added update and delete routers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,26 +23,39 @@ router.post("/api/post", function(req, res) {
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update(
+  burger.updateOne(
     {
-      sleepy: req.body.sleepy
+      devoured: req.body.devoured
     },
     condition,
     function(result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
+      } else {
+        res.status(200).end();
       }
+    });
+  });
+
+
+router.deleteOne(condition, function(req, res) {
+  var condition = "id = " +  req.params.id;
+  console.log("condition", condition);
+
+  burger.deleteOne(condition, function(result) {
+    if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
       res.status(200).end();
-
     }
-  );
+  });
 });
-
 // Export routes for server.js to use.
 module.exports = router;
