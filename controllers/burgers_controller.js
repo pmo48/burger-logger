b4c92c4b2fa696ba48db2f1d4cf3e869fd87427d
pmo48 <pmoriarty100@gmail.com>
2021-01-08 15:11:10 -0800

    added get route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,15 +1,15 @@
 var express = require("express");
+var burger = require("../models/burger.js");
 
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
