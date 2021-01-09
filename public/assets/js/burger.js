408b5b897016491c85a041870bc6236217ada767
pmo48 <pmoriarty100@gmail.com>
2021-01-09 15:30:48 -0800

    added javavscript file/fuctionality

---
+++ public/assets/js/burger.js
@@ -0,0 +1,52 @@
+$(function () {
+    $(".create-form").on("submit",function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newburger").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function() {
+            console.log("added new burger");
+            location.reload();
+        })
+    });
+
+    $(".eatburger").on("click", function(event){
+        event.preventDefault();
+        
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function() {
+            console.log("Burger devoured");
+            location.reload();
+        })
+    });
+
+    $(".trashburger").on("click", function(event){
+        event.preventDefault();
+        
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
+
+        $.ajax({
+            type: "DELETE",
+            url: "/api/burgers/" + id
+        }).then(function() {
+            console.log("Burger deleted");
+            location.reload();
+        })
+    });
+
+});
