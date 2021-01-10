e3a5a7ff0acf440699df5c3c81c44e299b42474c
pmo48 <pmoriarty100@gmail.com>
2021-01-10 08:18:47 -0800

    added JAWS connection logic

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host     : process.env.DB_HOST,
-  port     : 3306,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_NAME
-});
-
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    port     : 3306,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME
+  });
+}
 
 
 connection.connect(function(err) {
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,9 +3,6 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
@@ -18,7 +15,6 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
@@ -35,7 +31,6 @@ router.put("/api/burgers/:id", function(req, res) {
     condition,
     function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
@@ -50,12 +45,11 @@ router.delete("/api/burgers/:id", function(req, res) {
 
   burger.deleteOne(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
     }
   });
 });
-// Export routes for server.js to use.
+
 module.exports = router;
